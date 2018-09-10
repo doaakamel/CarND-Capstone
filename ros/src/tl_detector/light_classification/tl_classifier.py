e5ba495738ec8cbc92cb0a661c7415368e32fd16
vtretyakov <vtretyakov@synapticon.com>
2018-09-10 22:59:06 +0200

    add traffic light color detection
    * so far only annotating video stream
    * there is much noice coming from traffic lights on the other side of the road being identified

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -58,7 +58,7 @@ class TLClassifier(object):
         #tl_cropped = None
         for idx, classID in enumerate(classes):
             if classID == 10: #10 is traffic light
-                if scores[idx] > 0.40: #confidence level
+                if scores[idx] > 0.50: #confidence level
 
                     nbox = boxes[idx]
 
@@ -66,7 +66,57 @@ class TLClassifier(object):
                     width = image.shape[1]
 
                     box = np.array([nbox[0]*height, nbox[1]*width, nbox[2]*height, nbox[3]*width]).astype(int)
-                    #tl_cropped = image[box[0]:box[2], box[1]:box[3]]
+                    tl_cropped = image[box[0]:box[2], box[1]:box[3]]
+                    tl_color = self.get_color(tl_cropped)
                     #augment image with detected TLs
                     cv2.rectangle(image, (box[1], box[0]), (box[3], box[2]), (0, 255, 0), 2)
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    font_color = (0, 0, 0)
+                    cv2.putText(image, tl_color, (box[1], box[0]), font, 1.0, font_color, lineType=cv2.LINE_AA)
         return image
+
+    def get_color(self, image_rgb):
+        image_lab = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2LAB)
+        l = image_lab.copy()
+        # set a and b channels to 0
+        l[:, :, 1] = 0
+        l[:, :, 2] = 0
+
+        std_l = self.standardize_input(l)
+
+        red_slice, yellow_slice, green_slice = self.slice_image(std_l)
+
+        y, x, c = red_slice.shape
+        px_sums = []
+        color = ['RED', 'YELLOW', 'GREEN']
+        px_sums.append(np.sum(red_slice[0:y, 0:x, 0]))
+        px_sums.append(np.sum(yellow_slice[0:y, 0:x, 0]))
+        px_sums.append(np.sum(green_slice[0:y, 0:x, 0]))
+
+        max_value = max(px_sums)
+        max_index = px_sums.index(max_value)
+
+        return color[max_index]
+
+    def crop_n_blur(self, image):
+        row = 2
+        col = 6
+        blured_img = image.copy()
+        blured_img = blured_img[row:-row, col:-col, :]
+        blured_img = cv2.GaussianBlur(blured_img, (3, 3), 0)
+        return blured_img
+
+    def standardize_input(self, image):
+        standard_img = np.copy(image)
+        standard_img = cv2.resize(standard_img, (32, 32))
+        standard_img = self.crop_n_blur(standard_img)
+        return standard_img
+
+    def slice_image(self, image):
+        img = image.copy()
+        shape = img.shape
+        slice_height = shape[0] / 3
+        upper = img[0:slice_height, :, :]
+        middle = img[slice_height:2 * slice_height, :, :]
+        lower = img[2 * slice_height:3 * slice_height, :, :]
+        return upper, middle, lower
